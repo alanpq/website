e4dd34cabc3a1ca33d91960b3554f6d9aded1a0d
cakyman <alanpanayotov@gmail.com>
2020-12-05 03:52:47 +0000

    dockerize + basic web server

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM rust:1.48
+
+WORKDIR /website
+COPY dummy.rs .
+COPY Cargo.toml .
+RUN sed -i 's#src/main.rs#dummy.rs#' Cargo.toml
+RUN cargo build --release
+RUN sed -i 's#dummy.rs#src/main.rs#' Cargo.toml
+COPY . .
+
+RUN cargo install --path .
+
+EXPOSE 8080
+CMD ["website"]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use log::{info, warn};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Webserver running!");
+    HttpServer::new(|| App::new().service(hello).service(echo))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
